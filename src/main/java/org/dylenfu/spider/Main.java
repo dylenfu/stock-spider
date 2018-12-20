713aaacfde92d3d44a4faac2d5066252c31c439c
dylen <dylen@fukundeMacBook-Pro.local>
2018-12-20 14:21:39 +0800

    add typesafe for config

--- src/main/java/org/dylenfu/spider/Main.java
+++ src/main/java/org/dylenfu/spider/Main.java
@@ -4,13 +4,17 @@ import org.dylenfu.spider.accessor.*;
 import org.dylenfu.spider.data.*;
 import org.dylenfu.spider.converter.*;
 import org.jsoup.nodes.Document;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.Config;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        String dir = "/Users/dylen/workspace/javahome/github.com/dylenfu/stock-spider/data/";
-        String url = "http://stockpage.10jqka.com.cn/002648/";
+        Config config = ConfigFactory.load();
+        List<String> urls = config.getStringList("urls");
+        String dir = config.getString("cache");
 
         StockAccessor accessor = new StockAccessor();
         accessor.setCacheDir(dir);
@@ -18,11 +22,14 @@ public class Main {
         Converter converter = new StockConverter();
 
         try {
-            Document doc = accessor.getDocFromUrl(url);
-            Stock stock = new Stock();
-            converter.convert(doc, stock);
-
-            System.out.println(stock);
+            Iterator<String> iterator = urls.iterator();
+            while(iterator.hasNext()) {
+                String url = iterator.next();
+                Document doc = accessor.getDocFromUrl(url);
+                Stock stock = new Stock();
+                converter.convert(doc, stock);
+                System.out.println(stock);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
